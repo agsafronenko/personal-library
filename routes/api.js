83694d44ca1190b214c1a2dc8dd215bea5703cbb
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-08-17 15:07:24 +0200

    final api

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,83 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+const bookModel = require("../mongo_db/mongo_models").book;
 
 module.exports = function (app) {
+  app
+    .route("/api/books")
+    .get(function (req, res) {
+      bookModel.find({}, (err, library) => {
+        if (err) return console.log("error while trying to get personal library");
+        library = library.map((book) => {
+          return {
+            _id: book._id,
+            title: book.title,
+            commentcount: book.comments.length,
+          };
+        });
+        res.json(library);
+      });
+    })
 
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
+    .post(function (req, res) {
       let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      if (!title) return res.send("missing required field title");
+      let newBook = new bookModel({
+        title: title,
+        comments: [],
+      });
+      newBook.save((err, data) => {
+        if (err) return console.log("error while to trying to submit new book");
+        res.json({ _id: data._id, title: title });
+      });
     })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+
+    .delete(function (req, res) {
+      bookModel.remove({}, (err, data) => {
+        if (err) return console.log("error while trying to delete library");
+        res.send("complete delete successful");
+      });
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      bookModel.findById(bookid, (err, bookData) => {
+        if (err || !bookData) return res.send("no book exists");
+        res.json(bookData);
+      });
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
-      //json res format same as .get
+      bookModel.findById(bookid, (err, bookData) => {
+        if (err || !bookData) return res.send("no book exists");
+        if (!comment) return res.send("missing required field comment");
+        bookData.comments.push(comment);
+        bookData.save((err, data) => {
+          if (err) return console.log("error while trying to save new comment");
+          res.json(bookData);
+        });
+      });
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+      bookModel.findById(bookid, (err, bookData) => {
+        if (err || !bookData) return res.send("no book exists");
+        bookData.remove((err, data) => {
+          if (err) return console.log("error while trying to delete book data by book id");
+          res.send("delete successful");
+        });
+      });
     });
-  
 };
